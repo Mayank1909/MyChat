c60a60fba2c775f71c687676e4c2f82d33eb7d59
Mayank1909 <78377998+Mayank1909@users.noreply.github.com>
2024-01-31 13:57:41 +0530

    first commit

---
+++ chatserver/src/main/java/com/mayankprojectchat/chatserver/model/Message.java
@@ -0,0 +1,17 @@
+package com.mayankprojectchat.chatserver.model;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Message {
+    private String senderName;
+    private String receiverName;
+    private String message;
+    private String date;
+    private String status;
+}
